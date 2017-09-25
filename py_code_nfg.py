f4d0ebaa520337a3db6317557dc7cb3a832779fe
stas-g <grins38@gmail.com>
2017-09-25 14:27:45 +0100

    it works!

--- py_code_nfg.py
+++ py_code_nfg.py
@@ -1,4 +1,4 @@
-#source /home/participant/Documents/mscogs/mscogs/bin/activate
+#source /home/participant/Documents/mscogs/bin/activate
 
 import rpy2.robjects as robjects
 import numpy as np
@@ -7,12 +7,18 @@ from rpy2.robjects import pandas2ri
 pandas2ri.activate()
 
 robjects.r['load']("snpmod-fixed2.RData")
-u = robjects.r['SM2']
-
-for i in range(len(u))
 z = robjects.r('as.data.frame(attributes(SM2[[1]])$models)')
-#dataframe
-w = pandas2ri.ri2py(z)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
